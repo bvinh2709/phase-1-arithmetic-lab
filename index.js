cf21021fbe49d32c934097f6f56f815873577672
Vinh Nguyen <nguyenlebaovinh.27@gmail.com>
2023-01-31 17:11:00 -0600

    Complete arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+let num1 = 31, num2 = 2;
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10) + 1;
+Number.isInteger(random);
+
+let num3 = 10, num4 = 6;
+const mod = num3 % num4;
+
+const max = Math.max(0, 1, 4, 20, 9)
